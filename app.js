94e9f9544074f9dff984c8b0a4447d609db20159
lukinhapdm <lucaspintodemagalhaes@hotmail.com>
2023-02-09 16:53:05 -0300

    Adicionar interatividade com JavaScript

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function(){
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if (className == 'light-theme'){
+        this.textContent = "Escuro";
+    }
+    else {
+        this.textContent = "Claro";
+    }
+
+    console.log('Nome da classe atual: ' + className);
+
+});
